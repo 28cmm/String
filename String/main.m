ac0f494b2e57f37f1374368f561cec6c35ddfebc
28cmm <joshuafang1998@gmail.com>
2018-12-24 09:34:49 -0800

    finishhed

--- String/main.m
+++ String/main.m
@@ -10,8 +10,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSString * myName = @"Josh";
+        NSUInteger number = [myName length];
+        NSString *madLib = @"Yesterday, _PERSON_ and I when to the park. On our way to the _ADJECTIVE_1 park, we saw a _ADJECTIVE_2 _NOUN_ on a bike. We also saw big _ADJECTIVE_2 balloons tied to the _NOUN_. Once we got to the _ADJECTIVE_1 park, the sky turned _ADJECTIVE_2. It started to _VERB_. _PERSON_ and I _VERB_ all the way home.";
+        
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_PERSON_" withString:@"Josh"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_ADJECTIVE_1" withString:@"good"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_ADJECTIVE_2" withString:@"fast"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_NOUN_" withString:@"dog"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_VERB_" withString:@"run"];
+        
+        NSLog(@"%@", madLib);
+        NSLog(@"Hello %@",myName);
+        NSLog(@"Hello %lu",(unsigned long)number);
     }
     return 0;
 }
